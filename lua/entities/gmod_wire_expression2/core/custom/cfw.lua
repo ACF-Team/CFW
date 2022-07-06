b8a1de5c37facab3384bf359f05f3300df08ee43
Stoob <steejerbs@gmail.com>
2022-07-06 13:15:34 -0500

    Added support for E2

---
+++ lua/entities/gmod_wire_expression2/core/custom/cfw.lua
@@ -0,0 +1,87 @@
+E2Lib.RegisterExtension("contraption", true, "Enables interaction with Contraption Framework")
+
+local function isValidContraption(c)
+    return CFW.contraptions[c] or false
+end
+
+do -- Datatype and operator
+    registerType("contraption", "xcr", nil, nil, nil,
+        function(retval)
+            if retval == nil then return end
+            if not istable(retval) then error("Return value is neither nil nor a table, but a "..type(retval).."!",0) end
+        end,
+        function(v)
+            return not istable(v)
+        end
+    )
+
+    registerOperator("ass", "xcr", "xcr", function(self, args)
+        local lhs, op2, scope = args[2], args[3], args[4]
+        local      rhs        = op2[1](self, op2)
+
+        self.Scopes[scope][lhs]      = rhs
+        self.Scopes[scope].vclk[lhs] = true
+
+        return rhs
+    end)
+
+    e2function number operator_is(contraption cont)
+	    return isValidContraption(cont) and 1 or 0
+    end
+
+    e2function number operator==(contraption c1, contraption c2)
+        return c1 == c2 and 1 or 0
+    end
+
+    e2function number operator!=(contraption c1, contraption c2)
+        return c1 ~= c2 and 1 or 0
+    end
+end
+
+do
+    hook.Add("cfw.contraption.created", "e2Tables", function(c)
+        c.e2Table = E2Lib.newE2Table()
+    end)
+
+    -- TODO: Merge support
+end
+
+__e2setcost(5)
+
+e2function number contraption:isValid()
+    return isValidContraption(this) and 1 or 0
+end
+
+e2function contraption entity:getContraption()
+    return this:GetContraption()
+end
+
+e2function number contraption:count()
+    return isValidContraption(this) and this.count or 0
+end
+
+e2function number contraption:getMass()
+    return isValidContraption(this) and this.totalMass or 0
+end
+
+e2function table contraption:getTable()
+    return isValidContraption(this) and this.e2Table or E2Lib.newE2Table()
+end
+
+__e2setcost(20)
+
+e2function contraption contraption:getEntities()
+    if not isValidContraption(this) then return {} end
+
+    local output = {}
+    local count  = 0
+
+    for k in pairs(this.ents) do
+        count = count + 1
+
+        output[count] = k
+    end
+
+    self.prf = self.prf + count * 2
+    return output
+end
