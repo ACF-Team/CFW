cb51183fdcaaabe6ec82268914a8cc5691352821
Steve <5084958+Stooberton@users.noreply.github.com>
2025-11-12 23:25:56 -0600

    Fixed GetAABB
    Uses all 8 entity corners instead of GetPos

--- lua/cfw/extensions/position_sv.lua
+++ lua/cfw/extensions/position_sv.lua
@@ -1,6 +1,5 @@
 local CLASS     = CFW.Classes.Contraption
 local VEC_0     = Vector(0, 0, 0)
-local HUGE      = math.huge
 
 function CLASS:GetPos()
     -- TODO: Optimize this
@@ -11,29 +10,51 @@ function CLASS:GetPos()
         pos = pos + ent:GetPos()
     end
 
-    return pos / table.Count(self.ents)
+    return pos / self.count
 end
 
-function CLASS:GetAABB()
-    local mins, maxs = Vector(HUGE, HUGE, HUGE), -Vector(HUGE, HUGE, HUGE)
+do -- AABB
+    local HUGE   = math.huge
+    local corner = Vector()
 
-    for ent in pairs(self.ents) do
-        local pos = ent:GetPos()
+    local function expandAABB(ent, x, y, z, mins, maxs)
+        corner:SetUnpacked(x, y, z)
 
-        if pos.x < mins.x then mins.x = pos.x end
-        if pos.y < mins.y then mins.y = pos.y end
-        if pos.z < mins.z then mins.z = pos.z end
+        local worldCorner = ent:LocalToWorld(corner)
 
-        if pos.x > maxs.x then maxs.x = pos.x end
-        if pos.y > maxs.y then maxs.y = pos.y end
-        if pos.z > maxs.z then maxs.z = pos.z end
+        if worldCorner.x < mins.x then mins.x = worldCorner.x end
+        if worldCorner.y < mins.y then mins.y = worldCorner.y end
+        if worldCorner.z < mins.z then mins.z = worldCorner.z end
+        if worldCorner.x > maxs.x then maxs.x = worldCorner.x end
+        if worldCorner.y > maxs.y then maxs.y = worldCorner.y end
+        if worldCorner.z > maxs.z then maxs.z = worldCorner.z end
     end
 
-    local center = (mins + maxs) * 0.5
+    function CLASS:GetAABB()
+        local mins, maxs = Vector(HUGE, HUGE, HUGE), -Vector(HUGE, HUGE, HUGE)
 
-    debugoverlay.Cross(mins, 12, 0.03, Color(255, 0, 0), true)
-    debugoverlay.Cross(maxs, 12, 0.03, Color(0, 255, 0), true)
-    debugoverlay.Box(center, mins - center, maxs - center, 0.03, self.color)
+        for ent in pairs(self.ents) do
+            local obbMins, obbMaxs = ent:GetCollisionBounds()
+            local minX, minY, minZ = obbMins.x, obbMins.y, obbMins.z
+            local maxX, maxY, maxZ = obbMaxs.x, obbMaxs.y, obbMaxs.z
 
-    return mins, maxs, center
+            -- Calculate all 8 corners of the entity's OBB in world space
+            expandAABB(ent, maxX, minY, minZ, mins, maxs) -- Top Left Front
+            expandAABB(ent, maxX, minY, maxZ, mins, maxs) -- Top Left Back
+            expandAABB(ent, maxX, maxY, minZ, mins, maxs) -- Top Right Front
+            expandAABB(ent, maxX, maxY, maxZ, mins, maxs) -- Top Right Back
+            expandAABB(ent, minX, minY, minZ, mins, maxs) -- Bottom Left Front
+            expandAABB(ent, minX, minY, maxZ, mins, maxs) -- Bottom Left Back
+            expandAABB(ent, minX, maxY, minZ, mins, maxs) -- Bottom Right Front
+            expandAABB(ent, minX, maxY, maxZ, mins, maxs) -- Bottom Right Back
+        end
+
+        local center = (mins + maxs) * 0.5
+
+        debugoverlay.Cross(mins, 12, 0.03, Color(255, 0, 0), true)
+        debugoverlay.Cross(maxs, 12, 0.03, Color(0, 255, 0), true)
+        debugoverlay.Box(center, mins - center, maxs - center, 0.03, self.color)
+
+        return mins, maxs, center
+    end
 end
